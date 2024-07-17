3e5589eb760c8a00e25e62c93bacea670ce6ce64
Pedro Fortunato <pedro.fortunato.89@gmail.com>
2024-07-17 14:23:24 +0100

    Registr requires activation

--- api/template.go
+++ api/template.go
@@ -1,9 +1,15 @@
 package api
 
+type RegisterMail struct {
+	User string
+	Link string
+}
+
 var (
-	registerSubject = `Welcome to the Blog {{.UserName}}. Please verify the account.`
+	registerSubject = `Welcome to the Blog {{.User}}. Please verify the account.`
 
 	registerBody = `
-	We're glad you could join us {{.UserName}}. Please click the link below to verify your account:
+	We're glad you could join us {{.User}}. Please click the link below to verify your account:
+	{{.Link}}
 	`
 )
