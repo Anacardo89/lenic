4a40057ffc3e0fd1a10d37c1a0df101b8c7d4448
Pedro Fortunato <pedro.fortunato.89@gmail.com>
2024-07-17 11:29:21 +0100

    Fixes edit buttons not responding

--- static/js/all.js
+++ static/js/all.js
@@ -4,23 +4,26 @@ title.addEventListener('click', function() {
     location.href = "/home";
 })
 
-let visiv = false;
-coment_editor = document.getElementById('comment-editor-button');
-coment_editor.addEventListener('click', function() {
-    edit_form = document.getElementById('comment-editor')
-    if (!visiv){
-        edit_form.style.display = 'block';
-    } else{
-        edit_form.style.display = 'none';
-    }
-    visiv = !visiv;
-})
+comment_buttons = document.getElementsByClassName('comment-editor-button')
+for (let i = 0; i < comment_buttons.length; i++) {
+    const button = comment_buttons[i];
+    button.addEventListener('click', function() {
+    const id = comment_buttons[i].getAttribute('data-id');
+    editor_id = 'comment-editor-'+id
+    const edit_form = document.getElementById(editor_id);
+        if (edit_form.style.display === 'none' || edit_form.style.display === '') {
+            edit_form.style.display = 'block';
+        } else {
+            edit_form.style.display = 'none';
+        }
+    })
+ }
+
 
 function putComment(el) {
-
-    var id = $(el).find('.comment_id').val();
-    var guid = $(el).find('.post_guid').val();
-    var edited_comment = $(el).find('.edit_comment').val();
+    let id = $(el).find('.comment_id').val();
+    let guid = $(el).find('.post_guid').val();
+    let edited_comment = $(el).find('.edit_comment').val();
     $.ajax({
         url: '/api/post/' + guid + '/comment/' + id,
         method: 'PUT',
