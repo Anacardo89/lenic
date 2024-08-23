6f4406b46467c1d926910bb8a95d74e17e4bf75b
anacardo <pedro.fortunato.89@gmail.com>
2024-08-23 17:12:42 +0100

    Cleans up frontend for post and index, delete comment with modal

--- cmd/static/js/all.js
+++ cmd/static/js/all.js
@@ -1,55 +1,17 @@
 
-comment_buttons = document.getElementsByClassName('comment-editor-button')
-for (let i = 0; i < comment_buttons.length; i++) {
-    const button = comment_buttons[i];
-    button.addEventListener('click', function() {
-    const id = comment_buttons[i].getAttribute('data-id');
-        const comment_text_id = 'comment-text-'+id;
-        const comment_editor_id = 'comment-editor-'+id;
-        const edit_form = document.getElementById(comment_editor_id);
-        const comment_text = document.getElementById(comment_text_id);
-        if (edit_form.style.display === 'none' || edit_form.style.display === '') {
-            edit_form.style.display = 'block';
-            comment_text.style.display = 'none';
-        } else {
-            edit_form.style.display = 'none';
-            comment_text.style.display = 'block';
-        }
-    })
- }
-
-
-function putComment(el) {
-    let id = $(el).find('.comment_id').val();
-    let guid = $(el).find('.post_guid').val();
-    let edited_comment = $(el).find('.edit_comment').val();
+// Logout
+function logout(el) {
     $.ajax({
-        url: '/action/post/' + guid + '/comment/' + id,
-        method: 'PUT',
-        data: ({
-            comment: edited_comment
-        }),
+        url: '/action/logout',
+        method: 'POST',
         success: function(res) {
-            location.reload()
+            window.location.href = '/home';
         }
     })
     return false;
 }
 
-function deleteComment(el) {
-    let commentDiv = $(el).closest('.comment');
-    let id = commentDiv.find('.comment_id').val();
-    let guid = $(el).closest('.post_guid').val();
-    $.ajax({
-        url: '/action/post/' + guid + '/comment/' + id,
-        method: 'DELETE',
-        success: function(res) {
-            location.reload()
-        }
-    })
-    return false;
-}
-
+// Add a post
 function postPost() {
     let form = $('.post-form form')[0];
     let formData = new FormData(form)
@@ -69,13 +31,87 @@ function postPost() {
     return false;
 }
 
-function logout(el) {
+// Edit comment button behaviour
+comment_buttons = document.getElementsByClassName('comment-editor-button')
+for (let i = 0; i < comment_buttons.length; i++) {
+    const button = comment_buttons[i];
+    button.addEventListener('click', function() {
+    const id = comment_buttons[i].getAttribute('data-id');
+        const comment_text_id = 'comment-text-'+id;
+        const comment_editor_id = 'comment-editor-'+id;
+        const edit_form = document.getElementById(comment_editor_id);
+        const comment_text = document.getElementById(comment_text_id);
+        if (edit_form.style.display === 'none' || edit_form.style.display === '') {
+            edit_form.style.display = 'block';
+            comment_text.style.display = 'none';
+        } else {
+            edit_form.style.display = 'none';
+            comment_text.style.display = 'block';
+        }
+    })
+ }
+
+function putComment(el) {
+    let id = $(el).find('.comment_id').val();
+    let guid = $(el).find('.post_guid').val();
+    let edited_comment = $(el).find('.edit_comment').val();
     $.ajax({
-        url: '/action/logout',
-        method: 'POST',
+        url: '/action/post/' + guid + '/comment/' + id,
+        method: 'PUT',
+        data: ({
+            comment: edited_comment
+        }),
         success: function(res) {
-            window.location.href = '/home';
+            location.reload()
         }
     })
     return false;
+}
+
+
+// Delete comment button behaviour
+function deleteComment(el) {
+    let id = el.find('.comment_id').val();
+    let guid = $(el).closest('.post_guid').val();
+    $.ajax({
+        url: '/action/post/' + guid + '/comment/' + id,
+        method: 'DELETE',
+        success: function(res) {
+            location.reload()
+        }
+    })
+    return false;
+}
+
+const modal = document.getElementById("modal-container");
+var modalCancelBtn = document.getElementById("delete-sure-no");
+let commentIdToDelete = null;
+document.querySelectorAll('.comment-deleter-button').forEach(function(button) {
+    button.onclick = function() {
+        commentIdToDelete = this.getAttribute('data-id');
+        modal.style.display = "block";
+    }
+});
+
+modalCancelBtn.onclick = function() {
+    modal.style.display = "none";
+    commentIdToDelete = null;
+}
+
+document.getElementById('delete-sure-yes').onclick = function() {
+    if (commentIdToDelete !== null) {
+        var commentElement = $('.comment[data-id="' + commentIdToDelete + '"]');
+        if (commentElement) {
+            deleteComment(commentElement);
+        }
+        modal.style.display = "none"; // Hide the modal
+        commentIdToDelete = null; // Reset the comment ID
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        commentIdToDelete = null; // Reset the comment ID
+    }
 }
