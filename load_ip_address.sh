79d8069486c5de1d96505214aa35fe50abc3a1ca
anacardo <pedro.fortunato.89@gmail.com>
2024-08-23 20:37:23 +0100

    script to load hosts to config

---
+++ load_ip_address.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+db_name=mysql
+rabbit_name=some-rabbit
+network_name=bridge
+db_file="./config/dbConfig.yaml"
+rabbit_file="./config/rabbitConfig.yaml"
+
+db_ip=$(docker inspect $db_name | jq -r ".[0].NetworkSettings.Networks.$network_name.IPAddress")
+rabbit_ip=$(docker inspect $rabbit_name | jq -r ".[0].NetworkSettings.Networks.$network_name.IPAddress")
+
+yq eval --inplace ".dbHost = \"$db_ip\"" $db_file
+yq eval --inplace ".rabbit_host = \"$rabbit_ip\"" $rabbit_file
